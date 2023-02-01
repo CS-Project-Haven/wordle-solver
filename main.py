27573c4120fb582c3ed99f2bc213ccb7d22a25a1
SPARTAN-042 <sentinal042@gmail.com>
2023-02-01 00:13:56 +0000

    simple random word generator

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import itertools
+import random as rand
+
 """
 
 == Wordle Solver ==
@@ -6,7 +9,19 @@
 
 
 def start():
-    pass
+
+    # file_name = "valid_words.txt"
+    #
+    # rng = rand.Random(0)
+    # line_count = sum(1 for _ in open(file_name))
+    # random_index0 = rng.randint(0, line_count - 1)
+    # random_line = next(itertools.islice(open(file_name), random_index0, None))
+    #
+    # print(random_line)
+
+    with open("valid_words.txt") as f:
+        lines = f.readlines()
+        print(rand.choice(lines))
 
 
 if __name__ == "__main__":
